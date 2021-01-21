ff0a8b5a7aaef8ffe724d2804a9febca8d719c44
Ethan May <ethaniel.may@gmail.com>
2021-01-21 16:32:08 -0700

    Broken Star Hover Function Added

--- reviews/review.js
+++ reviews/review.js
@@ -1,8 +1,35 @@
 starArray = []
 
 function highlightStar(starNum) {
-    for (let i = 1; i < 6; i++) {
-
+    switch (starNum) {
+        case 1:
+            star = document.getElementById("star-1").classList.add("checked")
+            break;
+        case 2:
+            document.getElementById("star-1").classList.add("checked")
+            document.getElementById("star-2").classList.add("checked")
+            break;
+        case 3:
+            document.getElementById("star-1").classList.add("checked")
+            document.getElementById("star-2").classList.add("checked")
+            document.getElementById("star-3").classList.add("checked")
+            break;
+        case 4:
+            document.getElementById("star-1").classList.add("checked")
+            document.getElementById("star-2").classList.add("checked")
+            document.getElementById("star-3").classList.add("checked")
+            document.getElementById("star-4").classList.add("checked")
+            break;
+        case 5:
+            document.getElementById("star-1").classList.add("checked")
+            document.getElementById("star-2").classList.add("checked")
+            document.getElementById("star-3").classList.add("checked")
+            document.getElementById("star-4").classList.add("checked")
+            document.getElementById("star-5").classList.add("checked")
+            break;
+        default:
+            console.log("error on highlightStar()")
+            break;
     }
     console.log(starArray)
 }
